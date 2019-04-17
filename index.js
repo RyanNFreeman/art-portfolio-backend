80f3a1e5998bb1909bfd5282c5e9d5540d0cb717
Home <ryan.norman.freeman@gmail.com>
2019-04-17 14:28:18 -0600

    refactoring

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const port = 4000
-
 const server = require('./api/server')
+require('dotenv').config();
+
+const port = 4000 || process.env.PORT;
 
 server.get('/', (req, res) => {
     res.status(200).send('hello')
