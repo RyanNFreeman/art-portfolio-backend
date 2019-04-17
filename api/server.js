76b77f631d6d1e7092c3710967428882042d3c71
Home <ryan.norman.freeman@gmail.com>
2019-04-17 17:34:23 -0600

    troubleshooting

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const server = express()
 
+const userRouter = require("../routes/users_router");
+const postRouter = require("../routes/posts_router.js");
+
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
 server.get('/api', (req, res) => {
     res.status(200).send('this will be an endpoint')
 })
